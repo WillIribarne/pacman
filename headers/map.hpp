45aa1d54fb5ab1e45566290491861bd269149768
Your Name <iriguille0504@gmail.com>
2024-10-09 14:45:42 -0300

    map tweaks

--- headers/map.hpp
+++ headers/map.hpp
@@ -4,6 +4,7 @@ const unsigned int ROWS = 31;
 const unsigned int COLUMNS = 28;
 float X_TILE_SIZE = ANCHO_VENT / COLUMNS;
 float Y_TILE_SIZE = ALTO_VENT / ROWS;
+
 const char mapSketch[ROWS][COLUMNS + 1] = {
         "############################",
         "#            ##            #",
@@ -36,22 +37,32 @@ const char mapSketch[ROWS][COLUMNS + 1] = {
         "# ########## ## ########## #",
         "#                          #",
         "############################",
-    };
+};
+
+Vector2f currentTilePosition(int i, int k){
+    return {((int) X_TILE_SIZE % COLUMNS / 2) + X_TILE_SIZE * k, ((int) Y_TILE_SIZE % ROWS / 2) + Y_TILE_SIZE * i};
+}
 
 int colorMap(){
     for (int i = 0; i < ROWS; i++){
         for (int k = 0; k < COLUMNS; k++){
             RectangleShape rec = RectangleShape ({X_TILE_SIZE, Y_TILE_SIZE});
+            rec.setPosition(currentTilePosition(i,k));
             if (mapSketch[i][k] == '#'){
                 rec.setFillColor(Color(3, 15, 219)); //azul
-            } else if (mapSketch[i][k] == ' '){
-               rec.setFillColor(Color(255, 255, 255)); //blanco
+            } else if (mapSketch[i][k] == ' ' || mapSketch[i][k] == '.'){
+                rec.setFillColor(Color(0, 0, 0)); //negro
+                if (mapSketch[i][k] == '.'){
+                    CircleShape cir = CircleShape(5);
+                    cir.setFillColor(Color(255, 255, 255));
+                    cir.setPosition(currentTilePosition(i,k));
+                    ventana.draw(cir);
+                }
             } else if (mapSketch[i][k] == 'X'){
                 rec.setFillColor(Color(255, 227, 0)); //amarillo
-            } else {
+            } else if (mapSketch[i][k] == '|' || mapSketch[i][k] == '_'){
                 rec.setFillColor(Color(255, 0, 0)); //rojo
             }
-            rec.setPosition({((int) X_TILE_SIZE % COLUMNS / 2) + X_TILE_SIZE * k, ((int) Y_TILE_SIZE % ROWS / 2) + Y_TILE_SIZE * i});
             ventana.draw(rec);
         }
     }
--- headers/window.hpp
+++ headers/window.hpp
@@ -1,7 +1,7 @@
 #include "sfml-utn-inspt.h"
 
 const unsigned int ANCHO_VENT = 1080;
-const unsigned int ALTO_VENT = 768;
+const unsigned int ALTO_VENT = 900;
 const unsigned int FRAMERATE = 60;
 RenderWindow ventana = RenderWindow({ANCHO_VENT, ALTO_VENT}, "Utenian Pacman");
 
