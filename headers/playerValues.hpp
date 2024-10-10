d6ae7064490222cf7e74912ec76d710db993ccc1
Your Name <iriguille0504@gmail.com>
2024-10-10 12:03:17 -0300

    introduced OOP for pacman & window

--- headers/playerValues.hpp
+++ headers/playerValues.hpp
@@ -1,13 +1,33 @@
 #include "sfml-utn-inspt.h"
 
-CircleShape pacmanito = CircleShape(20);
-float velocidad;
-Vector2f diff;
+class Pacman {
+    private:
+        CircleShape shape;
+        float speed;
+        Vector2f pacmanMove;
+    public:
+        Pacman(){
+            shape = CircleShape(20);
+            speed = 3.f;
+            pacmanMove = {0,0};
+            setPacmanColor(Color(255, 242, 36));
+        }
+        void setPacmanColor(Color color){
+            shape.setFillColor(color);
+        }
+        void setSpeed(float spd){
+            speed = spd;
+        }
+        void setMovement(Vector2f vec){
+            pacmanMove = vec;
+        }
+};
 
-int setPlayerValues(){
+
+/*int setPlayerValues(){
     pacmanito.setFillColor(Color(255, 242, 36));
     pacmanito.setPosition({515.f, 675.f});
     velocidad = 3.f;
     diff = {0,0};
     return 1;
-}
+} */
--- headers/window.hpp
+++ headers/window.hpp
@@ -1,6 +1,18 @@
 #include "sfml-utn-inspt.h"
 
-const unsigned int ANCHO_VENT = 1080;
+class Window {
+    private:
+        const unsigned int ANCHO_VENT = 1080;
+        const unsigned int ALTO_VENT = 900;
+        const unsigned int FRAMERATE = 60;
+        RenderWindow window = RenderWindow({ANCHO_VENT, ALTO_VENT}, "Utenian Pacman");
+    public:
+        Window(){
+            window.setFramerateLimit(FRAMERATE);
+        }
+};
+
+/*const unsigned int ANCHO_VENT = 1080;
 const unsigned int ALTO_VENT = 900;
 const unsigned int FRAMERATE = 60;
 RenderWindow ventana = RenderWindow({ANCHO_VENT, ALTO_VENT}, "Utenian Pacman");
@@ -8,5 +20,5 @@ RenderWindow ventana = RenderWindow({ANCHO_VENT, ALTO_VENT}, "Utenian Pacman");
 int setWindow(){
     ventana.setFramerateLimit(FRAMERATE);
     return 1;
-}
+} */
 
